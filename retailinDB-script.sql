6f3496193eddd9e252d4ae5d8a2d0927af62c180
Will <jiepeng.huwu@sjsu.edu>
2017-12-01 13:36:35 -0800

    db script

--- retailinDB-script.sql
+++ retailinDB-script.sql
@@ -1,18 +1,1 @@
-CREATE DATABASE `retailinWebAppDB`;
-Use retailinWebAppDB
-
-DROP TABLE IF EXISTS `retailinWebAppDB`
-CREATE TABLE IF NOT EXISTS `retailinWebAppDB`.`users` (
-  `emp_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(40) NOT NULL,
-  `last_name` VARCHAR(40) NOT NULL,
-  `first_name` VARCHAR(40) NOT NULL,
-  `password` VARCHAR(100) NOT NULL,
-  `email` VARCHAR(60) NOT NULL,
-  `phone_number` VARCHAR(12) NOT NULL,
-  `enabled` TINYINT(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`emp_id`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC))
-ENGINE = InnoDB
-AUTO_INCREMENT = 20
-DEFAULT CHARACTER SET = utf8
+use retailingWebAppDB;
---
+++ retailinWebAppDB.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE  IF NOT EXISTS `retailinWebAppDB`;
+USE `retailinWebAppDB`;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `customer` (
+  `emp_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `first_name` varchar(45) DEFAULT NULL,
+  `password` varchar(70) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `phone_number` varchar(12) DEFAULT NULL,
+  `enabled` boolean DEFAULT '1',
+  PRIMARY KEY (`emp_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+LOCK TABLES `customer` WRITE;
+INSERT INTO `customer` VALUES 
+	(1,'David','Adams','david@luv2code.com'),
+	(2,'John','Doe','john@luv2code.com'),
+	(3,'Ajay','Rao','ajay@luv2code.com'),
+	(4,'Mary','Public','mary@luv2code.com'),
+	(5,'Maxwell','Dixon','max@luv2code.com');
+UNLOCK TABLES;
